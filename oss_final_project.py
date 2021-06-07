de1fa813369e5afe06fe81475d1226dbf029e582
jo-wick <21600862@handong.edu>
2021-06-08 02:12:12 +0900

    final commit

--- oss_final_project.py
+++ oss_final_project.py
@@ -13,12 +13,7 @@ import matplotlib.pyplot as plt
 # In[75]:
 
 
-# 채무 gap 평균 증가량을 (21개 기준)으로 평균 값을 낸 뒤
-# 2020년 채무 gdp에 더한 뒤 나눴을 때 60%가 넘는지 확인한뒤
-# 넘지 않는다면 2021년 채무 gdp를 더해 다시 평균 증가량을 구해준뒤
-# 다시 나눠 60%가 넘는지 확인한 뒤 60%가 넘을 때까지 확인한다.
 
-# 그런 다음 linear regression을 이용하여 그래프 등을 그린다!
 
 
 # In[95]:
@@ -35,9 +30,6 @@ df.head()
 
 
 from sklearn.model_selection import train_test_split
-
-#train data X = (현재까지 평균 gdp, 현재까지 평균 채무, 현재 gdp, 현재 채무)
-#train data Y = (다음 년도 gdp, 다음 년도 채무)
 
 x = df[['가구원수','가구주연령', '소득분위']]
 y = df[['소득']]
@@ -57,24 +49,6 @@ lr.fit(x_train, y_train)
 
 
 y_predict = lr.predict(x_test)
-# y_predict1 = y_predict
-# y_predict2 = y_predict
-# print(y_predict)
-# print(y_test)
-# # print(y_predict)
-# print(y_predict.size)
-# range1 = y_predict.size/2
-# range1 = int(range1)
-# y_year = []
-# y_rate = []
-# print(range1)
-# for i in range(1, range1+1):
-#     y_year = np.append(y_year, y_predict[i-1][1])
-#     y_rate = np.append(y_rate, y_predict[i-1][0])
-# print(y_year.size)
-# print(y_rate.size)
-# print(y_year)
-# print(y_rate)
 
 
 # In[93]:
@@ -125,7 +99,7 @@ temp = []
 temp.append(result)
 my_predict = lr.predict(temp)
 my_predict = int(my_predict)
-print(my_predict, '원')
+print(my_predict, 'won')
 
 
 # In[ ]:
